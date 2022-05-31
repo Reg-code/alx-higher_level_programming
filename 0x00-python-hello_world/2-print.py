eab9352f31195fcb8bdf739ae8ed83a90a29354e
Reg-code <rebeccadurojaiye1@gmail.com>
2022-05-31 09:06:17 +0000

    file

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,6 +1,2 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-print("\"programming is like building a multilingual puzzles")
-=======
-print("\"Programming is like building a multilingual puzzle")
->>>>>>> b811d8a0ae65ce8c5b5dbdc30e3ef81ac4ef013a
+print('"Programming is like building a multilingual puzzle')
