9901b7909e12dbddcb7941fb77d8fc5d73e2947c
Reg-code <rebeccadurojaiye1@gmail.com>
2022-05-31 08:30:34 +0000

    file

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,9 +1,8 @@
-
-rd = "Holberton"
+#!/usr/bin/python3
+word = "Holberton"
 word_first_3 = word[:3]
 word_last_2 = word[-2:]
-middle_word = word[1:-1]
+middle_word = word[1:len(word) -1]
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
 print(f"Middle word: {middle_word}")
-
