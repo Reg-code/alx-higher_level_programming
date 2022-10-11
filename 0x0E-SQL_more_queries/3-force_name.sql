717edae6b6016e700a03af45b0b2c2a96920c376
reg-code <rebeccadurojaiye1@gmail.com>
2022-10-11 05:05:56 -0700

    file

--- 0x0E-SQL_more_queries/3-force_name.sql
+++
@@ -1,2 +0,0 @@
--- creates the table force_name on MySQL server.
-CREATE TABLE IF NOT EXISTS `force_name` (`id` INT, `name` VARCHAR(256) NOT NULL);
--- 0x0E-SQL_more_queries/5-unique_id.sql
+++
@@ -1,6 +0,0 @@
--- creates the table unique_id on MySQL server.
-
-CREATE TABLE IF NOT EXISTS unique_id (
-    `id` INT UNIQUE DEFAULT 1,
-    `name` VARCHAR(256) NOT NULL
-);
